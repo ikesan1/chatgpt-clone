8becbbe1692e4d349eb849a239e5f20c8f2d159e
ikesan <ilkerciliv@gmail.com>
2024-10-21 17:24:59 +0100

    working initialization of backend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,12 @@
 import express from "express";
 
-console.log("Hello from the backend!!!!!!");
 const app = express();
-
 const port = process.env.PORT || 3000;
+
+app.get("/api/upload", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
