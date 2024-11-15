97ef4732a5ff4e92cf93e0287adeb46856721b71
ikesan <ilkerciliv@gmail.com>
2024-11-15 15:58:05 +0000

    chat model schema added

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,6 @@ app.use(
 
 const connect = async () => {
   try {
-    console.log("Connecting to MongoDB...");
     await mongoose.connect(process.env.MONGO_URI);
     console.log(
       `Successfully Connected to MongoDB\nServer running on port ${port}`
